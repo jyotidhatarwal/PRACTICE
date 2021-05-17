0f27cdbcd7d54c843a71cd77bbac043d26a6bdfa
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-05-17 19:36:51 +0530

    Update questions.java

--- Trees/questions.java
+++ Trees/questions.java
@@ -81,6 +81,34 @@ public ListNode build(ListNode head, int n){
     return root;
 }
 
+/*   Delete Node in a BST (LC - 450)   */
+
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null) return null;
+        if(key > root.val){
+            root.right = deleteNode(root.right,key);
+        }else if(key < root.val){
+            root.left = deleteNode(root.left,key);
+        }else{
+            root = delete(root);
+        }
+        return root;
+    }
+    private TreeNode delete(TreeNode node){
+        if(node.left == null && node.right == null) return null;
+        else if(node.right == null) return node.left;
+        else if(node.left == null) return node.right;
+        else{
+            TreeNode rootp1 = node.right;
+            while(rootp1.left != null){
+                rootp1 = rootp1.left;
+            }
+            rootp1.left = node.left;
+            return node.right;
+        }
+    }
+}
 
 
 
