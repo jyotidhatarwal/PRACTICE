901a9cad66427e7ce2b3f74ccc5bbf06dfdd92bd
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-05-20 21:26:11 +0530

    Update Questions.java

--- Graphs/Questions.java
+++ Graphs/Questions.java
@@ -302,4 +302,82 @@ public class Main {
 	}
 	
 }
+	
+	/*MINIMUM SPANNING TREE USING PRIMS ( SPOJ) */
+import java.io.*;
+import java.util.*;
+
+public class Main {
+   static class Edge {
+      int src;
+      int nbr;
+      int wt;
+
+      Edge(int src, int nbr, int wt) {
+         this.src = src;
+         this.nbr = nbr;
+         this.wt = wt;
+      }
+   }
+    public static class Pair implements Comparable<Pair>{
+       int v;
+       int av;
+       int wt;
+       Pair(int v,int av,int wt){
+           this.v = v;
+           this.av = av;
+           this.wt = wt;
+       }
+       public int compareTo(Pair other){
+           return this.wt - other.wt;
+       }
+   }
+
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       String s=br.readLine();
+       String[] sp=s.split(" ");
+      int vtces = Integer.parseInt(sp[0]);
+      ArrayList<Edge>[] graph = new ArrayList[vtces];
+      for (int i = 0; i < vtces; i++) {
+         graph[i] = new ArrayList<>();
+      }
+
+      int edges = Integer.parseInt(sp[1]);
+      for (int i = 0; i < edges; i++) {
+         String[] parts = br.readLine().split(" ");
+         int v1 = Integer.parseInt(parts[0])-1;
+         int v2 = Integer.parseInt(parts[1])-1;
+         int wt = Integer.parseInt(parts[2]);
+         graph[v1].add(new Edge(v1, v2, wt));
+         graph[v2].add(new Edge(v2, v1, wt));
+      }
+
+      // write your code here
+      PriorityQueue<Pair> pq = new PriorityQueue<>();
+      long[] ans = new long[vtces];
+      boolean[] visited = new boolean[vtces];
+      pq.add(new Pair(0,-1,0));
+      while(pq.size() >0){
+          Pair rem = pq.remove();
+          if(visited[rem.v] == true) continue;
+          visited[rem.v] = true;
+          ans[rem.v] = rem.wt;
+      
+         for(Edge e:graph[rem.v]){
+              if(visited[e.nbr] == false){
+                  pq.add(new Pair(e.nbr,rem.v,e.wt));
+              }
+          }
+      }
+      long sum =0;
+      for(int i=0;i<ans.length;i++){
+          sum += ans[i];
+          
+      }
+      System.out.println(sum);
+   }
+  
+
+}
 
