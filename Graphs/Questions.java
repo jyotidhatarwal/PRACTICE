f3b0f58d75fdbfb2e6310317ec8f29bca1bb8a30
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-05-21 20:50:18 +0530

    Update Questions.java

--- Graphs/Questions.java
+++ Graphs/Questions.java
@@ -380,4 +380,45 @@ public class Main {
   
 
 }
+	
+	/*	BELLMAN FORD ALGORITHM	*/
+	
 
+	
+	
+	/*	Negative weight cycle  (GFG)	*/
+
+class Solution
+{
+    public int isNegativeWeightCycle(int n, int[][] edges)
+    {
+        //code here
+        int[] path = new int[n];
+        Arrays.fill(path,Integer.MAX_VALUE);
+        path[0] = 0;
+        
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<edges.length;j++){
+                int u = edges[j][0];
+                int v = edges[j][1];
+                int wt = edges[j][2];
+                if(path[u] == Integer.MAX_VALUE) continue;
+                if(path[u] + wt < path[v]){
+                    path[v] = path[u] + wt;
+                }
+            }
+        }
+        for(int j=0;j<edges.length;j++){
+            int u = edges[j][0];
+            int v = edges[j][1];
+            int wt = edges[j][2];
+            if(path[u] == Integer.MAX_VALUE) continue;
+            if(path[u] + wt < path[v]){
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
+	
+	
