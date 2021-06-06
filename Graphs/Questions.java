8e7a69fc84155787939be94bfc89901dac6f4699
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-06-06 15:38:40 +0530

    Update Questions.java

--- Graphs/Questions.java
+++ Graphs/Questions.java
@@ -2158,6 +2158,38 @@ class Solution {
 
 	
 	
+	/*	Floyd Warshall	ALGORITHM	T-O(V^3)	*/
+	
+	
+
+class Solution
+{
+    public void shortest_distance(int[][] matrix)
+    {
+        // Code here 
+         int V = matrix.length;
+        for(int k=0;k<V;k++){
+            for(int i=0;i<V;i++){
+                for(int j=0;j<V;j++){
+                    if(i == j || i == k || j == k) continue;
+                     if(matrix[i][k] != -1 && matrix[k][j] != -1){
+	                    if(matrix[i][j] != -1){
+	                        matrix[i][j] = Math.min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+			    }
+	                    else{
+	                        matrix[i][j] = matrix[i][k] + matrix[k][j];
+			    }
+	                }
+                }
+            }
+        }
+    }
+}
+	
+	
+	
+	
+	
 	
 	
 	
