f15236a842c5a368a0e4ac0c82523baf90d9ed37
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-07-14 00:22:23 +0530

    Update questions.java

--- Searching and Sorting/questions.java
+++ Searching and Sorting/questions.java
@@ -498,5 +498,44 @@ class Solution {
 }
 
 
+/*  Search in Rotated Sorted Array II (LC-81)   */
 
+
+class Solution {
+    public boolean search(int[] nums, int target) {
+        
+        int low = 0;
+        int hi = nums.length-1;
+        
+        while(low <= hi){
+            // to avoid duplicates
+            while(low < hi && nums[low] == nums[low+1]){
+                low++;
+            }
+            while(low < hi && nums[hi] == nums[hi-1]){
+                hi--;
+            }
+          
+          // search in sorted rotated array
+            int mid = low + (hi - low)/2;
+            
+            if(nums[mid] == target){
+                return true;
+            }else if(nums[low] <= nums[mid]){
+                if(target >= nums[low] && nums[mid] > target){
+                    hi = mid-1;
+                }else{
+                    low = mid+1;
+                }
+            }else if(nums[mid] <= nums[hi]){
+                if(target > nums[mid] && nums[hi] >= target){
+                    low = mid+1;
+                }else{
+                    hi = mid-1;
+                }
+            }
+        }
+        return false;
+    }
+}
 
