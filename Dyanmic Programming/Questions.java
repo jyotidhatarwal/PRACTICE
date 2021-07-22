b2be75864a814e7d0f8b76c8c0e4f5b583b45479
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-07-22 11:19:45 +0530

    Update Questions.java

--- Dyanmic Programming/Questions.java
+++ Dyanmic Programming/Questions.java
@@ -398,7 +398,29 @@ class Solution {
 // T-O(NLOGN) APPROACH
 
 
-
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[]dp = new int[nums.length];
+        int ans =0;
+        for(int val : nums){
+            int i =0;
+            int j = ans;
+            while(i < j){
+                int mid = i + (j-i)/2;
+                if(dp[mid] < val){
+                    i = mid+1;
+                }else{
+                    j = mid;
+                }
+            }
+            dp[i] = val;
+            if(i == ans){
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
 
 
 
