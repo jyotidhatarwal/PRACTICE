9b0f0b6b21bffe61d45982fb93198d6db4164588
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-07-23 15:16:12 +0530

    Update Questions.java

--- Dyanmic Programming/Questions.java
+++ Dyanmic Programming/Questions.java
@@ -724,3 +724,42 @@ class Solution {
 }
 
 
+
+//	MATRIX CHAIN MULTIPLICATION	(GFG)
+
+
+// NAIVE APPROACH T-O(N^3)
+
+
+class Solution{
+    static int matrixMultiplication(int N, int arr[])
+    {
+        // code here
+        int[][]dp = new int[N-1][N-1];
+        for(int gap=0;gap<dp.length;gap++){
+            for(int i=0,j=gap;j<dp.length;i++,j++){
+                if(gap == 0){
+                    dp[i][j] =0;
+                }else if(gap == 1){
+                    dp[i][j] = arr[i]*arr[j]*arr[j+1];
+                }else{
+                    int min = Integer.MAX_VALUE;
+                    for(int k=i;k<j;k++){
+                        int leftCost = dp[i][k];
+                        int rightCost = dp[k+1][j];
+                        int multiplicationCost = arr[i]*arr[k+1]*arr[j+1];
+                        int totalCost = leftCost + rightCost + multiplicationCost;
+                        if(totalCost < min){
+                            min = totalCost;
+                        }
+                    }
+                    dp[i][j] = min;
+                }
+            }
+        }
+        return dp[0][dp.length-1];
+    }
+}
+
+
+
