1cd4bca379b60d345d500a1d9d3f03c57a2f1d97
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-07-23 11:41:54 +0530

    Update questions.java

--- Trees/questions.java
+++ Trees/questions.java
@@ -10,6 +10,53 @@ class Node{
     }
 }
 
+*/
+
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~` BINARY TREE TRAVERSALS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+// INORDER TRAVERSAL (MORRIS)
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+      List<Integer> ans = new ArrayList<>();
+        while(root != null){
+            if(root.left == null){
+                ans.add(root.val);
+                root = root.right;
+            }else{
+                TreeNode rootp1 = root.left;
+                while(rootp1.right != null && rootp1.right != root){
+                    rootp1 = rootp1.right;
+                }
+                if(rootp1.right == null){
+                    rootp1.right = root;
+                    root = root.left;
+                }else{
+                    ans.add(root.val);
+                    rootp1.right = null;
+                    root = root.right;
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 	/*		Left View of Binary Tree	(GFG)		*/
