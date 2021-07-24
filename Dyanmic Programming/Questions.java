55db946159f62bc4b21e3027092932a62596ebdf
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-07-24 14:45:30 +0530

    Update Questions.java

--- Dyanmic Programming/Questions.java
+++ Dyanmic Programming/Questions.java
@@ -819,6 +819,47 @@ public class Solution {
 }
 
 
+//	Optimal binary search tree 	(GFG)
+
+
+// NAIVE APPROACH - T-O(N^3)
+
+
+class Solution
+{
+    static int optimalSearchTree(int keys[], int freq[], int n)
+    {
+        // code here
+        int[][]dp = new int[n][n];
+        for(int gap =0;gap<n;gap++){
+            for(int i=0,j=gap;j<n;i++,j++){
+                if(gap == 0){
+                    dp[i][j] = freq[i];
+                }else if(gap == 1){
+                    int freq1 = freq[i];
+                    int freq2 = freq[j];
+                    dp[i][j] = Math.min(freq1 + 2*freq2,2*freq1 + freq2);
+                }else{
+                    int min = Integer.MAX_VALUE;
+                    int freqSum =0;
+                    for(int x=i;x<=j;x++){
+                        freqSum += freq[x];
+                    }
+                    for(int k=i;k<=j;k++){
+                        int left = k == i ? 0 : dp[i][k-1];
+                        int right = k == j ? 0 : dp[k+1][j];
+                        min = Math.min(min,left+right+freqSum);
+                    }
+                    dp[i][j] = min;
+                }
+            }
+        }
+        return dp[0][n-1];
+    }
+}
+
+
+
 
 
 
