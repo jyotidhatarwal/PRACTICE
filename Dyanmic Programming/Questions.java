f23b5eb140f3b0ff80380808c5b82e0ca836093a
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-07-25 19:38:06 +0530

    Update Questions.java

--- Dyanmic Programming/Questions.java
+++ Dyanmic Programming/Questions.java
@@ -1006,5 +1006,77 @@ class Solution {
 
 
 
+//	Longest Palindromic Substring	(LC-5)
+
+
+class Solution {
+    public String longestPalindrome(String s) {
+     int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        String ans = "";
+        for(int gap=0;gap<n;gap++){
+            for(int i=0,j=gap;j<n;i++,j++){
+                if(gap == 0){
+                    dp[i][j] = true;
+                }else if(gap == 1){
+                    if(s.charAt(i) == s.charAt(j)){
+                        dp[i][j] = true;
+                    }else{
+                        dp[i][j] = false;
+                    }
+                }else{
+                    if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1] == true){
+                        dp[i][j] = true;
+                    }else{
+                        dp[i][j] = false;
+                    }
+                }
+                if(dp[i][j] == true){
+                    ans = s.substring(i,j+1);
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+
+
+//	Palindromic Substrings	(LC-647)
+
+
+class Solution {
+    public int countSubstrings(String s) {
+        int n = s.length();
+        boolean[][]dp = new boolean[n][n];
+        int count =0;
+        for(int gap =0;gap<n;gap++){
+            for(int i=0,j=gap;j<n;i++,j++){
+                if(gap == 0){
+                    dp[i][j] = true;
+                    count++;
+                }else if(gap == 1){
+                    if(s.charAt(i) == s.charAt(j)){
+                        dp[i][j] = true;
+                        count++;
+                    }else{
+                        dp[i][j] = false;
+                    }
+                }else{
+                    if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1] == true){
+                        dp[i][j] = true;
+                        count++;
+                    }else{
+                        dp[i][j] = false;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
+
+
+
 
 
